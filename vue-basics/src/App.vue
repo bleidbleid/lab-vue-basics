ce3b22e1ce5cb42f054901d545a6cc63faeb7f29
Maria Bladé <mariablade2000+git@gmail.com>
2022-10-11 17:49:33 +0200

    done fourth iteration

--- vue-basics/src/App.vue
+++ vue-basics/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <NavBar/>
   <Suma/>
+  <List/>
   <Footer/>
 </template>
 <script setup>
@@ -8,6 +9,7 @@ import { ref } from 'vue';
 import NavBar from './components/NavBar.vue';
 import Footer from './components/Footer.vue';
 import Suma from './components/Suma.vue';
+import List from './components/List.vue';
 </script>
 <style scoped>
 </style>
---
+++ vue-basics/src/components/List.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="section">
+        <div class="container">
+
+            <h1 class="title">Les Tres Bessones</h1>
+            <div class="columns is-multiline">
+                <div v-for="post in posts" class="column is-12-mobile is-6-tablet is-4-desktop">
+                    <Post :title="post.title" :description="post.description" :image="post.image" />
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script setup>
+import Post from './Post.vue';
+const posts = [
+    {
+        title: 'Helena',
+        description: 'La bessona verd',
+        image: 'https://pbs.twimg.com/amplify_video_thumb/1118749914471727104/img/p5QO4Wytd2yz6Hkp.jpg'
+    },
+    {
+        title: 'Anna',
+        description: 'La bessona de blau',
+        image: 'https://static.filmin.es/images/media/25747/1/card_0_3_1360x765.png'
+    },
+    {
+        title: 'Teresa',
+        description: 'La bessona de rosa',
+        image: 'https://i.ytimg.com/vi/ftOOygB1iOY/hqdefault.jpg'
+    }
+]
+</script>
+<style scoped>
+
+</style>
