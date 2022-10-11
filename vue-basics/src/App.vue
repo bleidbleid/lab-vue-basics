4e1f6598f156d686e309fd4dc2b0843bb5c7e0dc
Maria Bladé <mariablade2000+git@gmail.com>
2022-10-11 17:03:32 +0200

    done iteration3

---
+++ vue-basics/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <NavBar/>
+  <Suma/>
+  <Footer/>
+</template>
+<script setup>
+import { ref } from 'vue';
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+import Suma from './components/Suma.vue';
+</script>
+<style scoped>
+</style>
---
+++ vue-basics/src/components/Suma.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{2+2}}</h1>
+        <button @click="cool = !cool">Surprise!</button>
+        <h1 v-if="cool">This is a cool string!</h1>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const cool = ref(false);
+
+// function string() {
+
+//      cool.value = !cool.value;
+// ;
+
+// }
+</script>
+
+<style scoped>
+
+</style>
