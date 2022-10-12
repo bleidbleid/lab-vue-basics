c9ed26724dc84f3d2c8643d51e1c3e2aa2dbb7a9
Maria Bladé <mariablade2000+git@gmail.com>
2022-10-12 09:01:12 +0200

    done

--- vue-basics/src/components/List.vue
+++ vue-basics/src/components/List.vue
@@ -4,21 +4,22 @@
 
             <h1 class="title">Les Tres Bessones</h1>
             <div class="columns is-multiline">
-                <div v-for="post in posts" class="column is-12-mobile is-6-tablet is-4-desktop">
-                    <Post :title="post.title" :description="post.description" :image="post.image" />
+                <div v-for="(post, index) in posts" :key="index" class="column is-12-mobile is-6-tablet is-4-desktop" >
+                    <Post :title="post.title" :description="post.description" :image="post.image"/>
+                    
                 </div>
             </div>
-
         </div>
     </div>
 </template>
 <script setup>
+import { ref } from 'vue';
 import Post from './Post.vue';
 const posts = [
     {
         title: 'Helena',
         description: 'La bessona verd',
-        image: 'https://pbs.twimg.com/amplify_video_thumb/1118749914471727104/img/p5QO4Wytd2yz6Hkp.jpg'
+        image: 'https://pbs.twimg.com/amplify_video_thumb/1118749914471727104/img/p5QO4Wytd2yz6Hkp.jpg',
     },
     {
         title: 'Anna',
@@ -31,7 +32,24 @@ const posts = [
         image: 'https://i.ytimg.com/vi/ftOOygB1iOY/hqdefault.jpg'
     }
 ]
+const colorCard = ref(true);
+console.log(posts);
+
+
 </script>
 <style scoped>
+.color4 {
+    background-color: white;
+} .coloret0 {
+    background-color: yellowgreen;
+} 
+
+.coloret1 {
+    background-color: blue;
+} 
+
+.coloret2 {
+    background-color: violet;
+} 
 
 </style>
--- vue-basics/src/components/Post.vue
+++ vue-basics/src/components/Post.vue
@@ -5,25 +5,43 @@
                 <img :src="props.image" alt="Placeholder image">
             </figure>
         </div>
-        <div class="card-content">
+        <div class="card-content"  :class="colorCard ? 'color4' : 'coloret2'" >
             <div class="content">
                 <div class="card-title">{{props.title}}</div>
                 <div>{{props.body}}</div>
+                <h1>{{props.index}}</h1>
             </div>
-            <button class="button">Color!</button>
+            <button class="button" @click="colorCard = !colorCard">Color!</button>
         </div>
     </div>
 </template>
 <script setup>
+import { ref } from 'vue'
 import { defineProps } from 'vue'
 // Definimos los props que vamos a recibir en el componente
 const props = defineProps(['title', 'body', 'image']);
+const colorCard = ref(true);
+
 </script>
 <style scoped>
 .card{
     display: flex;
     flex-direction: column;
 }
+
+.color4 {
+    background-color: white;
+} .coloret0 {
+    background-color: yellowgreen;
+} 
+
+.coloret1 {
+    background-color: blue;
+} 
+
+.coloret2 {
+    background-color: violet;
+} 
 .card-content{
     flex: 1;
     display: flex;
